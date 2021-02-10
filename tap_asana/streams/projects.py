bc61af091c0dae505f6488c2727ec83deefc138f
Mckelle Carr <mckellecarr@Mckelle-Carrs-Macbook-Pro.local>
2021-02-09 16:42:55 -0800

    trying to trigger refresh faster

--- tap_asana/streams/projects.py
+++ tap_asana/streams/projects.py
@@ -36,12 +36,15 @@ class Projects(Stream):
 
 
   def get_objects(self):
+    iter = 0
     bookmark = self.get_bookmark()
     session_bookmark = bookmark
     opt_fields = ",".join(self.fields)
     for workspace in Context.asana.client.workspaces.find_all():
       for project in Context.asana.client.projects.find_all(workspace=workspace["gid"], opt_fields=opt_fields):
         session_bookmark = self.get_updated_session_bookmark(session_bookmark, project[self.replication_key])
+        if iter > 5:
+          Context.asana.refresh_access_token()
         if self.is_bookmark_old(project[self.replication_key]):
           yield project
     self.update_bookmark(session_bookmark)
