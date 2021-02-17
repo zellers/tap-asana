44f2b980c4387fcf0849ddcc44c0e015bcef79f7
Mckelle Carr <mckellecarr@Mckelle-Carrs-Macbook-Pro.local>
2021-02-17 14:26:52 -0800

    additional try catch

--- tap_asana/streams/tasks.py
+++ tap_asana/streams/tasks.py
@@ -1,6 +1,8 @@
 
 from singer import utils
 import singer
+from oauthlib.oauth2 import TokenExpiredError
+from tap_asana.streams.base import invalid_token_handler
 from tap_asana.context import Context
 from tap_asana.streams.base import Stream
 
@@ -56,12 +58,19 @@ class Tasks(Stream):
     session_bookmark = bookmark
     modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
     opt_fields = ",".join(self.fields)
-    for workspace in self.call_api("workspaces"):
-      for project in self.call_api("projects", workspace=workspace["gid"]):
-        for task in self.call_api("tasks", project=project["gid"], opt_fields=opt_fields, modified_since=modified_since): 
-          session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
-          if self.is_bookmark_old(task[self.replication_key]):
-            yield task
+    try:
+      for workspace in self.call_api("workspaces"):
+        for project in self.call_api("projects", workspace=workspace["gid"]):
+          for task in self.call_api("tasks", project=project["gid"], opt_fields=opt_fields, modified_since=modified_since):
+            session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
+            if self.is_bookmark_old(task[self.replication_key]):
+              yield task
+    except TokenExpiredError as TEE:
+      LOGGER.info("ATTENTION: Exception Caught in tasks get_objects", TEE)
+      invalid_token_handler()
+    except Exception as e:
+      LOGGER.info("ATTENTION: Generic exception caught in call_api", e)
+      invalid_token_handler()
     self.update_bookmark(session_bookmark)
 
 
