027894a6aa580bf9c5740e78d15c1124d6b5edd3
Mckelle Carr <mckellecarr@Mckelle-Carrs-Macbook-Pro.local>
2021-03-05 11:59:12 -0800

    add error handling to other streams and logging

--- tap_asana/streams/projects.py
+++ tap_asana/streams/projects.py
@@ -3,6 +3,8 @@ import singer
 from singer import utils
 from tap_asana.context import Context
 from tap_asana.streams.base import Stream
+from tap_asana.streams.base import invalid_token_handler
+from oauthlib.oauth2 import TokenExpiredError
 
 
 class Projects(Stream):
@@ -37,14 +39,24 @@ class Projects(Stream):
 
 
   def get_objects(self):
+    LOGGER = singer.get_logger()
+    LOGGER.info("ATTENTION: Starting Projects Sync")
     bookmark = self.get_bookmark()
     session_bookmark = bookmark
     opt_fields = ",".join(self.fields)
-    for workspace in Context.asana.client.workspaces.find_all():
-      for project in Context.asana.client.projects.find_all(workspace=workspace["gid"], opt_fields=opt_fields):
-        session_bookmark = self.get_updated_session_bookmark(session_bookmark, project[self.replication_key])
-        if self.is_bookmark_old(project[self.replication_key]):
-          yield project
+    try:
+      for workspace in Context.asana.client.workspaces.find_all():
+        for project in Context.asana.client.projects.find_all(workspace=workspace["gid"], opt_fields=opt_fields):
+          session_bookmark = self.get_updated_session_bookmark(session_bookmark, project[self.replication_key])
+          if self.is_bookmark_old(project[self.replication_key]):
+            yield project
+    except TokenExpiredError as TEE:
+      LOGGER.info("ATTENTION: TokenExpiredError exception caught in Projects get_objects")
+      invalid_token_handler(TEE)
+    except Exception as e:
+      LOGGER.info("ATTENTION: Generic exception caught in Projects get_objects")
+      invalid_token_handler(e)
+
     self.update_bookmark(session_bookmark)
 
 
--- tap_asana/streams/sections.py
+++ tap_asana/streams/sections.py
@@ -1,7 +1,10 @@
-
+import singer
 from singer import utils
 from tap_asana.context import Context
 from tap_asana.streams.base import Stream
+from tap_asana.streams.base import invalid_token_handler
+from oauthlib.oauth2 import TokenExpiredError
+
 
 
 class Sections(Stream):
@@ -19,14 +22,23 @@ class Sections(Stream):
 
 
   def get_objects(self):
+    LOGGER = singer.get_logger()
+    LOGGER.info("ATTENTION: Starting Sections Sync")
     bookmark = self.get_bookmark()
     session_bookmark = bookmark
     modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
     opt_fields = ",".join(self.fields)
-    for workspace in self.call_api("workspaces"):
-      for project in self.call_api("projects", workspace=workspace["gid"]):
-        for section in Context.asana.client.sections.get_sections_for_project(project_gid=project["gid"], owner="me", opt_fields=opt_fields):
-          yield section
+    try:
+      for workspace in self.call_api("workspaces"):
+        for project in self.call_api("projects", workspace=workspace["gid"]):
+          for section in Context.asana.client.sections.get_sections_for_project(project_gid=project["gid"], owner="me", opt_fields=opt_fields):
+            yield section
+    except TokenExpiredError as TEE:
+      LOGGER.info("ATTENTION: TokenExpiredError exception caught in Sections get_objects")
+      invalid_token_handler(TEE)
+    except Exception as e:
+      LOGGER.info("ATTENTION: Generic exception caught in Sections get_objects")
+      invalid_token_handler(e)
 
 
 Context.stream_objects['sections'] = Sections
