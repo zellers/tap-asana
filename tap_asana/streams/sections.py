eae0ebb6bef3f8f7e34489a55b4bc0cc87124445
Mckelle Carr <mckellecarr@Mckelle-Carrs-Macbook-Pro.local>
2021-03-10 14:50:40 -0800

    adjust timer logic

--- tap_asana/streams/sections.py
+++ tap_asana/streams/sections.py
@@ -7,41 +7,41 @@ from tap_asana.streams.base import invalid_token_handler
 from oauthlib.oauth2 import TokenExpiredError
 
 
-
 class Sections(Stream):
-  name = 'sections'
-  replication_method = 'FULL_TABLE'
+    name = 'sections'
+    replication_method = 'FULL_TABLE'
 
-  fields = [
-    "gid",
-    "resource_type",
-    "name",
-    "created_at",
-    "project",
-    "projects"
-  ]
+    fields = [
+        "gid",
+        "resource_type",
+        "name",
+        "created_at",
+        "project",
+        "projects"
+    ]
 
+    def get_objects(self):
+        LOGGER = singer.get_logger()
+        LOGGER.info("ATTENTION: Starting Sections Sync")
+        bookmark = self.get_bookmark()
+        session_bookmark = bookmark
+        modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        opt_fields = ",".join(self.fields)
 
-  def get_objects(self):
-    LOGGER = singer.get_logger()
-    LOGGER.info("ATTENTION: Starting Sections Sync")
-    bookmark = self.get_bookmark()
-    session_bookmark = bookmark
-    modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
-    opt_fields = ",".join(self.fields)
+        # Refreshing token at the start of sections
+        Context.asana.refresh_access_token()
+        start_timer = time.time()
 
-    #Refreshing token at the start of sections
-    Context.asana.refresh_access_token()
-    start_timer = time.time()
-
-    for workspace in self.call_api("workspaces"):
-      for project in self.call_api("projects", workspace=workspace["gid"]):
-        for section in Context.asana.client.sections.get_sections_for_project(project_gid=project["gid"], owner="me", opt_fields=opt_fields):
-          if (time.time() - start_timer) % 2700 == 0:
-            LOGGER.info("ATTENTION: 45 min passed since start of sections sync, refreshing token")
-            Context.asana.refresh_access_token()
-          yield section
-
+        for workspace in self.call_api("workspaces"):
+            for project in self.call_api("projects", workspace=workspace["gid"]):
+                for section in Context.asana.client.sections.get_sections_for_project(project_gid=project["gid"],
+                                                                                      owner="me",
+                                                                                      opt_fields=opt_fields):
+                    if (time.time() - start_timer) > 2700:
+                        LOGGER.info("ATTENTION: 45 min passed since start of sections sync, refreshing token")
+                        Context.asana.refresh_access_token()
+                        start_timer = time.time()  # start timer over
+                    yield section
 
 
 Context.stream_objects['sections'] = Sections
--- tap_asana/streams/tasks.py
+++ tap_asana/streams/tasks.py
@@ -1,4 +1,3 @@
-
 from singer import utils
 import singer
 from tap_asana.streams.base import invalid_token_handler
@@ -8,77 +7,77 @@ from oauthlib.oauth2 import TokenExpiredError
 import time
 
 
-
 class Tasks(Stream):
-  name = 'tasks'
-  replication_key = "modified_at"
-  replication_method = 'INCREMENTAL'
-  fields = [
-    "gid",
-    "resource_type",
-    "name",
-    "approval_status",
-    "assignee_status",
-    "completed",
-    "completed_at",
-    "completed_by",
-    "created_at",
-    "dependencies",
-    "dependents",
-    "due_at",
-    "due_on",
-    "external",
-    "hearted",
-    "hearts",
-    "html_notes",
-    "is_rendered_as_seperator",
-    "liked",
-    "likes",
-    "memberships",
-    "modified_at",
-    "notes",
-    "num_hearts",
-    "num_likes",
-    "num_subtasks",
-    "resource_subtype",
-    "start_on",
-    "assignee",
-    "custom_fields",
-    "followers",
-    "parent",
-    "permalink_url",
-    "projects",
-    "tags",
-    "workspace"
-  ]
+    name = 'tasks'
+    replication_key = "modified_at"
+    replication_method = 'INCREMENTAL'
+    fields = [
+        "gid",
+        "resource_type",
+        "name",
+        "approval_status",
+        "assignee_status",
+        "completed",
+        "completed_at",
+        "completed_by",
+        "created_at",
+        "dependencies",
+        "dependents",
+        "due_at",
+        "due_on",
+        "external",
+        "hearted",
+        "hearts",
+        "html_notes",
+        "is_rendered_as_seperator",
+        "liked",
+        "likes",
+        "memberships",
+        "modified_at",
+        "notes",
+        "num_hearts",
+        "num_likes",
+        "num_subtasks",
+        "resource_subtype",
+        "start_on",
+        "assignee",
+        "custom_fields",
+        "followers",
+        "parent",
+        "permalink_url",
+        "projects",
+        "tags",
+        "workspace"
+    ]
 
+    def get_objects(self):
+        LOGGER = singer.get_logger()
+        LOGGER.info("ATTENTION: Starting Tasks Sync")
+        bookmark = self.get_bookmark()
+        session_bookmark = bookmark
+        modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        opt_fields = ",".join(self.fields)
 
-  def get_objects(self):
-    LOGGER = singer.get_logger()
-    LOGGER.info("ATTENTION: Starting Tasks Sync")
-    bookmark = self.get_bookmark()
-    session_bookmark = bookmark
-    modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
-    opt_fields = ",".join(self.fields)
+        # Refreshing token at the start of tasks
+        Context.asana.refresh_access_token()
+        start_timer = time.time()
 
-    #Refreshing token at the start of tasks
-    Context.asana.refresh_access_token()
-    start_timer = time.time()
+        workspaces = self.call_api("workspaces")
+        for workspace in workspaces:
+            projects = self.call_api("projects", workspace=workspace["gid"])
+            for project in projects:
+                if (time.time() - start_timer) % 2700 == 0:
+                    LOGGER.info("ATTENTION: 45 min passed since start of task sync, refreshing token")
+                    Context.asana.refresh_access_token()
+                    start_timer = time.time()  # start timer over
 
-    workspaces = self.call_api("workspaces")
-    for workspace in workspaces:
-      projects = self.call_api("projects", workspace=workspace["gid"])
-      for project in projects:
-        if (time.time() - start_timer) % 2700 == 0:
-          LOGGER.info("ATTENTION: 45 min passed since start of task sync, refreshing token")
-          Context.asana.refresh_access_token()
-
-        tasks = self.call_api("tasks", project=project["gid"], opt_fields=opt_fields, modified_since=modified_since)
-        for task in tasks:
-          session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
-          if self.is_bookmark_old(task[self.replication_key]):
-            yield task
-    self.update_bookmark(session_bookmark)
+                tasks = self.call_api("tasks", project=project["gid"], opt_fields=opt_fields,
+                                      modified_since=modified_since)
+                for task in tasks:
+                    session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
+                    if self.is_bookmark_old(task[self.replication_key]):
+                        yield task
+        self.update_bookmark(session_bookmark)
 
 
 Context.stream_objects['tasks'] = Tasks
