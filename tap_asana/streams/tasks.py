8e55260c35f886a094a602642e48d09515f751da
Mckelle Carr <mckellecarr@Mckelle-Carrs-Macbook-Pro.local>
2021-03-17 12:05:14 -0700

    move timer to task level

--- tap_asana/streams/tasks.py
+++ tap_asana/streams/tasks.py
@@ -66,14 +66,13 @@ class Tasks(Stream):
         for workspace in workspaces:
             projects = self.call_api("projects", workspace=workspace["gid"])
             for project in projects:
-                if (time.time() - start_timer) > 1800:
-                    LOGGER.info("ATTENTION: 30 min passed, refreshing token")
-                    Context.asana.refresh_access_token()
-                    start_timer = time.time()  # start timer over
-
                 tasks = self.call_api("tasks", project=project["gid"], opt_fields=opt_fields,
                                       modified_since=modified_since)
                 for task in tasks:
+                    if (time.time() - start_timer) > 1800:
+                        LOGGER.info("ATTENTION: 30 min passed, refreshing token")
+                        Context.asana.refresh_access_token()
+                        start_timer = time.time()  # start timer over
                     session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
                     if self.is_bookmark_old(task[self.replication_key]):
                         yield task
